7e0639cd12685f2e42efbffd584abf55238d60e9
ervandil <ervandilcunha@hotmail.com>
2025-02-02 14:51:00 -0300

    fixed alerts

--- 06_ParametersAndExceptionHandler/src/main/java/br/com/ervandil/controllers/MathController.java
+++ 06_ParametersAndExceptionHandler/src/main/java/br/com/ervandil/controllers/MathController.java
@@ -1,6 +1,5 @@
 package br.com.ervandil.controllers;
 
-import java.util.concurrent.atomic.AtomicLong;
 
 import br.com.ervandil.exceptions.UnsupportedMathOperationException;
 import br.com.ervandil.math.SimpleMath;
@@ -10,37 +9,35 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class MathController {
 
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
     private final SimpleMath simpleMath = new SimpleMath();
 
     @RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+    public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
         return simpleMath.performOperation(numberOne, numberTwo, (Double::sum));
     }
 
     @RequestMapping(value = "/subtraction/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double subtraction(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+    public Double subtraction(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
         return simpleMath.performOperation(numberOne, numberTwo, ((a, b) -> a - b));
     }
 
     @RequestMapping(value = "/multiplication/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double multiplication(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+    public Double multiplication(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
         return simpleMath.performOperation(numberOne, numberTwo, ((a, b) -> a * b));
     }
 
     @RequestMapping(value = "/division/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double division(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+    public Double division(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
         return simpleMath.performOperation(numberOne, numberTwo, ((a, b) -> a / b));
     }
 
     @RequestMapping(value = "/average/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-    public Double average(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+    public Double average(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
         return simpleMath.performOperation(numberOne, numberTwo, ((a, b) -> (a + b) / 2));
     }
 
     @RequestMapping(value = "/squareRoot/{numberOne}", method = RequestMethod.GET)
-    public Double squareRoot(@PathVariable(value = "numberOne") String numberOne) throws Exception {
+    public Double squareRoot(@PathVariable(value = "numberOne") String numberOne) {
         if (!MathUtil.isNumeric(numberOne)) {
             throw new UnsupportedMathOperationException("Please set a numeric value");
         }
